612e3b9c975bb685f0ce640ad3e9b30697d35b6a
rjspencer <rjspencer82@gmail.com>
2014-09-27 19:02:38 +0000

    behavior controller

---
+++ app/controllers/behaviors_controller.rb
@@ -0,0 +1,11 @@
+class BehaviorsController < ApplicationController
+  def new
+    @groups = Group.all
+    render 'new'
+  end
+  
+  def create
+    Behavior.create(params)
+    redirect_to :new_behavior
+  end
+end
---
+++ test/controllers/behaviors_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BehaviorsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
